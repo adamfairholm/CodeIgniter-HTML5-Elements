ec572f46843cfa24815c1abd30134167d870f1ae
Adam Fairholm <adam.fairholm@gmail.com>
2010-06-20 00:13:16 -0400

    Basic helper setup. First function added, form_email.

---
+++ application/helpers/MY_Form_helper.php
@@ -0,0 +1,62 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * CodeIgniter HTML5 Form Helper
+ *
+ * Extended from the CodeIgniter native HTML helper
+ * @copyright	Copyright (c) 2006, EllisLab, Inc.
+ * @link		http://codeigniter.com
+ *
+ * Description:
+ *
+ * This extends the form helper to do all the cool new HTML5 stuff.
+ *
+ * @copyright 	Copyright (c) Adam Fairholm 2010
+ * @version 	1.0
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ **/
+
+// ------------------------------------------------------------------------
+
+/**
+ * Email Field
+ *
+ * Identical to the input function but adds the "email" type
+ *
+ * @access	public
+ * @param	mixed
+ * @param	string
+ * @param	string
+ * @return	string
+ */
+if ( ! function_exists('form_email'))
+{
+	function form_email($data = '', $value = '', $extra = '')
+	{
+		$defaults = array('type' => 'email', 'name' => (( ! is_array($data)) ? $data : ''), 'value' => $value);
+
+		return "<input "._parse_form_attributes($data, $defaults).$extra." />";
+	}
+}
+
+
+
+/* End of file MY_Form_helper.php */
+/* Location: ./application/helpers/MY_Form_helper.php */
